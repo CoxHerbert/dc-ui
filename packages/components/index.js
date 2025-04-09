3c6e188b088860b4e37af43eba7abb995df5600d
鲍泽楠 <coxherbert98@gmail.com>
2025-04-09 14:34:59 +0800

    1

--- packages/components/index.js
+++ packages/components/index.js
@@ -1,11 +1,21 @@
 import dcDict from './dc-dict/index.vue';
 import dcDictKey from './dc-dict-key/index.vue';
 import dcView from './dc-view/index.vue';
+import dcChat from './dc-chat/index.vue';
+import dcDateRange from './dc-date-range/index.vue';
+import dcDateRangePicker from './dc-date-range-picker/index.vue';
 
-let globalConfig = {};
+let globalConfig = {
+    // 组件数据options
+    cacheData: {},
+    // 请求方法
+    axios: null,
+    // vuex仓库
+    store: null,
+};
 
 const setGlobalConfig = (config) => {
-    globalConfig.cacheData = config?.cacheData;
+    globalConfig = config;
     console.log('setGlobalConfig:', globalConfig, 'config:', config);
 };
 
@@ -15,6 +25,9 @@ export default {
         app.component('dc-dict', dcDict);
         app.component('dc-dict-key', dcDictKey);
         app.component('dc-view', dcView);
+        app.component('dc-chat', dcChat);
+        app.component('dc-date-range', dcDateRange);
+        app.component('dc-date-range-picker', dcDateRangePicker);
         setGlobalConfig(config);
     },
 };
